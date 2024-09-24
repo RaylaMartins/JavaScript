d780392ce5bd11e061208a0383e3e6d49804f1a0
RaylaMartins <raylamartins123@outlook.com>
2024-09-24 11:55:15 -0300

    JavaScript
    Desenvolvendo em sala

---
+++ aula1.introdulçao/scripts/script.js
@@ -0,0 +1,18 @@
+function explicacao(){
+    // se () senao fimSe
+    if(true){
+        console.log("ESTOU NO IF")
+    }else{
+        console.log("ESTOU NO ELSE")
+    }
+
+    var idade = 20
+
+
+    if(idade <= 18) 
+     alert(Entrada permitida!)
+
+    else{
+        alert(Entrada nâo permitida!)
+    }
+}
